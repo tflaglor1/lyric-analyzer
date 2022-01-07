3a92ab9dc43ad1cf65f6dd444ba5702d74c5650f
Thomas Flaglor <tflaglor1@gmail.com>
2022-01-07 01:41:22 -0500

    Created getDiscog.js that gets track and album info from Spotify API

---
+++ src/getDiscog.js
@@ -0,0 +1,106 @@
+const SpotifyWebApi = require('spotify-web-api-node');
+const qs = require('qs');
+const axios = require('axios');
+require('dotenv').config({path: __dirname + '/../.env'});
+
+let clientId = process.env.CLIENT_ID;
+let clientSecret = process.env.CLIENT_SECRET;
+const auth_token = Buffer.from(`${clientId}:${clientSecret}`, 'utf-8').toString('base64');
+
+// sets up spotifyApi with clientId and clientSecret
+let spotifyApi = new SpotifyWebApi({
+    clientId: clientId,
+    clientSecret: clientSecret,
+});
+
+/**
+ * Gets access token from Spotify, which is needed for API requests
+ * @returns the data token
+ */
+async function getAuth(){
+    try{
+        const token_url = 'https://accounts.spotify.com/api/token';
+        const data = qs.stringify({'grant_type':'client_credentials'});
+
+        const response = await axios.post(token_url, data, {
+            headers: {
+                'Authorization': `Basic ${auth_token}`,
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        })
+
+        return response.data.access_token;
+    }catch(error){
+        console.log(error);
+    }
+}
+
+/**
+ * Gets all albums from and artist and removes duplicates
+ * @param {String} artist_id
+ */
+async function getAlbums(artist_id){
+    // 50 is limit on Spotify API
+    let data = await spotifyApi.getArtistAlbums(artist_id, {limit: 50, album_type: 'album', market: 'US'});
+
+    const items = data.body.items
+    let albums = new Map();
+    items.forEach(item => albums.set(item.name, item.id));
+    // go through duplicates and take the longer album, and usually since chronological, explicit is the first
+    // so grab first album, going to get deluxe albums and take out identical tracks later
+    let prevAlbum
+    // deletes duplicates
+    for(let [key, value] of albums){
+        if(value === prevAlbum){
+            albums.delete(key);
+        }
+        prevAlbum = value;
+    }
+    return albums;
+    
+}
+
+/**
+ * Gets all tracks from an Album and their id
+ * @param {String} albumID 
+ * @returns Map<Track Name, ID>
+ */
+async function getTracksFromAlbum(albumID){
+    let data = await spotifyApi.getAlbumTracks(albumID, {limit: 50});
+    const items = data.body.items;
+    let album_tracks = new Map();
+
+    for(track of items){
+        album_tracks.set(track.name, track.id);
+    }
+    return album_tracks;
+}
+
+/**
+ * Returns all tracks from all of artist's albums
+ * @param {Map<Album Name, ID>} discog 
+ * @returns Map<Track Name, ID> of all tracks
+ */
+async function getAllTracksFromDiscog(albums){
+    let tracks = new Map();
+    for(let [key, value] of albums){
+        album_tracks = await getTracksFromAlbum(value);
+        tracks = new Map([...tracks, ...album_tracks]); // combines map and stops duplicates
+    }
+    return tracks;
+}
+
+// tests
+/*
+(async () =>{
+    let access_code = await getAuth();
+    spotifyApi.setAccessToken(access_code);
+    console.log(spotifyApi.getAccessToken());
+    let albums = await getAlbums('5K4W6rqBFWDnAN6FQUkS6x'); // for testing
+    console.log(albums)
+    let tracks = await getTracksFromAlbum('2Wiyo7LzdeBCsVZiRA6vVZ');
+    console.log(tracks);
+    let all_tracks = await getAllTracksFromDiscog(albums) 
+    console.log(all_tracks);
+})();
+*/
